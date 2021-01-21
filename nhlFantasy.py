2b7ff98755c2b09636970456fefaa2047712ffe4
Riku <riku.hagg@edu.turkuamk.fi>
2021-01-22 01:30:10 +0200

    lisätty pisteiden lasku maaliavhdeille

--- nhlFantasy.py
+++ nhlFantasy.py
@@ -29,23 +29,37 @@ for j in ids:
         printPlayerName(name)
         
     else:
-        peopleInfo = json.loads((requests.get('https://statsapi.web.nhl.com/api/v1/people/'+j)).text)
+        peopleInfo = json.loads((requests.get('https://statsapi.web.nhl.com/api/v1/people/'+j)).text) #lataa pelajaan tiedot apista
         stats = json.loads((requests.get('https://statsapi.web.nhl.com/api/v1/people/'+j+'/stats?stats=statsSingleSeason&season=20202021')).text)
 
-        if peopleInfo['people'][0]['primaryPosition']['code'] == 'G':
-            print(True)
-
-        elif stats['stats'][0]['splits'] == []:
+        if peopleInfo['people'][0]['primaryPosition']['code'] == 'G': #tarkistaa onko kyseessä maalivahti
             goals = 0
             assists = 0
+            wins = stats['stats'][0]['splits'][0]['stat']['wins']
+            shutOuts = stats['stats'][0]['splits'][0]['stat']['shutouts']*2
+            pointsTable[name] += wins+shutOuts
+            print(peopleInfo['people'][0]['fullName'],'| W:',wins, '| SO: ',shutOuts, '| P:',(wins+shutOuts))
+
+            #Loppupisteytyksessä saatavat pisteet
+            svProsentti = stats['stats'][0]['splits'][0]['stat']['savePercentage']*100
+            gaa = stats['stats'][0]['splits'][0]['stat']['goalAgainstAverage']
+            loppupisteet = (svProsentti/gaa)/2
+            print(loppupisteet)
+
+
+        else:
+            if stats['stats'][0]['splits'] == []: 
+                goals = 0
+                assists = 0
             
-        else:     
-            goals = stats['stats'][0]['splits'][0]['stat']['goals']
-            assists = stats['stats'][0]['splits'][0]['stat']['assists']
-            points = goals + assists
-            pointsTable[name] += points
+            else:     
+                goals = stats['stats'][0]['splits'][0]['stat']['goals']
+                assists = stats['stats'][0]['splits'][0]['stat']['assists']
+                points = goals + assists
+                pointsTable[name] += points
             
-        print(peopleInfo['people'][0]['fullName'],'| G:',goals, '| A: ',assists, '| P:',(goals+assists))
+            print(peopleInfo['people'][0]['fullName'],'| G:',goals, '| A: ',assists, '| P:',(goals+assists))
         
-print(pointsTable)
+print()
+print(pointsTable) #tulostaa sen hetkisenkokonaispistetilanteen
  
